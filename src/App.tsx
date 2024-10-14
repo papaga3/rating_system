dc5132950e26baa85462de5d9cffccc069ee77d6
Huy Nguyen <ng.q.huy98@gmail.com>
2024-10-14 08:41:01 +0300

    update to ServiceComponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import service_data from "./dummy_data/services.json";
+import Grid from "@mui/joy/Grid";
 
 import { Service } from "@/types/types";
 import ServiceComponent from "./components/ServiceComponent";
@@ -12,14 +13,15 @@ function App() {
       <h1 className="text-3xl font-bold underline">
         Service List
       </h1>
-      <div className="">
+      <Grid container spacing={2} sx={{ flexGrow: 1 }}>
         { services.map((item, index) => 
-          (<ServiceComponent 
-            key={`service-${index}-${item.id}`} 
-            service={item}/>
+          (
+            <Grid key={`service-${index}-${item.id}`}>
+              <ServiceComponent service={item} />
+            </Grid>
           ))
         }
-      </div>
+      </Grid>
     </div>
   );
 }
--- src/components/ServiceComponent.tsx
+++ src/components/ServiceComponent.tsx
@@ -1,4 +1,5 @@
-import { Card } from "@mui/joy";
+import { AspectRatio, Card, Typography } from "@mui/joy";
+import StarIcon from '@mui/icons-material/Star';
 
 import { Service } from "@/types/types";
 
@@ -8,9 +9,14 @@ interface Props {
 
 const ServiceComponent: React.FC<Props> = ( { service } ) => {
     return (
-        <Card sx={{ orientation:"horizontal", width: "10rem" }}>
-            name: {service.name}
-            rating: { service.avgRating }
+        <Card sx={{ display: "inline-block", width: "300px", height: "300px" }}>
+            <div>
+                <Typography level="title-lg" sx={ {height: "50px"} }> {service.name} </Typography>
+            </div>
+            <AspectRatio>
+
+            </AspectRatio>
+            <Typography> Average rating: { service.avgRating } <StarIcon sx={ {verticalAlign: "text-bottom"} } htmlColor="#ffbe33"/> </Typography>
         </Card>
     );
 }
